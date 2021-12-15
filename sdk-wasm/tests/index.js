bb0b5a1cd8fb17f334ba4c8a84e300fd2c6bb5e2
jiyuu-jin <zach@valist.io>
2021-12-14 20:03:26 -0500

    Add axios and WebAssembly.instantiate()

--- sdk-wasm/tests/index.js
+++ sdk-wasm/tests/index.js
@@ -1,22 +1,25 @@
-const assert = require("assert");
-const loader = require("@assemblyscript/loader");
 const Valist = require("@valist/sdk");
 const valist = new Valist({ web3Provider: "https://rpc.valist.io/mumbai", metaTx: false });
+const axios = require('axios');
 
 (async () => {
     await valist.connect();
-
     const release = await valist.getLatestRelease('acme-co', 'wasm-modules');
     console.log(release);
-    const req = await fetch(`https://gateway.valist.io${release.releaseCID}`);
-    const releaseJSON = await req.json();
+    try {
+        const releaseRes = await axios.get(`https://gateway.valist.io${release.releaseCID}`);
+        const releaseJSON = releaseRes.data;
+        console.log(releaseJSON);
 
-    await loader.instantiateStreaming(fetch(`https://gateway.valist.io${releaseJSON.artifacts['untouched.wasm'].provider}`))
-    .then(obj => obj.instance.exports.add(1,2));
-    // const { instance } = await loader.instantiateStreaming(, importObject);
-    // const result = instance.add(1, 2);
-    // assert.strictEqual(instance.add(1, 2), 3);
-    // console.log(instance.add(1, 2));
-    // console.log(result);
-    // console.log("ok");
+        const wasmRes = await axios.get(`https://gateway.valist.io${releaseJSON.artifacts['untouched.wasm'].provider}`);
+        const wasmBuffer = Buffer.from(wasmRes.data, 'binary');
+
+        WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+            const add = wasmModule.instance.exports.add;
+            const sum = add(992, 345);
+            console.log("992 + 345 is:", sum);
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
 })();
